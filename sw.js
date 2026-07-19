ea608febfd7357bbd51e659e1d6b7e7c459142e4
Chen Xiaolin <abiling@gmail.com>
2026-07-19 17:40:30 +0800

    Refresh PWA cache for world zoom

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'hk-coffee-map-v3-20260719c';
+const CACHE = 'hk-coffee-map-v3-20260719d';
 const SHELL = [
-  './', './index.html', './styles.css', './admin.css', './app.js', './admin-core.js', './manifest.webmanifest',
+  './', './index.html', './styles.css', './admin.css', './map-config.js', './app.js', './admin-core.js', './manifest.webmanifest',
   './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'
 ];
 
