5fe65f76e0819a13fc30d7867733575858606940
Chen Xiaolin <abiling@gmail.com>
2026-07-19 20:32:39 +0800

    Refresh cache for corrected administrator swipe UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hk-coffee-map-v3-20260719g';
+const CACHE = 'hk-coffee-map-v3-20260719h';
 const SHELL = [
   './', './index.html', './styles.css', './admin.css', './filter-scroll.css', './compact-nav.css', './map-config.js', './compact-search.js', './app.js', './admin-core.js', './manifest.webmanifest',
   './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'
