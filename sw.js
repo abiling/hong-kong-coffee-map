e0522b95336e37cbe393f2efd00090ab3c155abc
Chen Xiaolin <abiling@gmail.com>
2026-07-19 18:37:47 +0800

    Cache compact navigation and focused search assets

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'hk-coffee-map-v3-20260719f';
+const CACHE = 'hk-coffee-map-v3-20260719g';
 const SHELL = [
-  './', './index.html', './styles.css', './admin.css', './filter-scroll.css', './map-config.js', './app.js', './admin-core.js', './manifest.webmanifest',
+  './', './index.html', './styles.css', './admin.css', './filter-scroll.css', './compact-nav.css', './map-config.js', './compact-search.js', './app.js', './admin-core.js', './manifest.webmanifest',
   './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'
 ];
 
