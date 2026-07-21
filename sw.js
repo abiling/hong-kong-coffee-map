98eb7662b90f39529479ad65f1b7c0f077656d89
Chen Xiaolin <abiling@gmail.com>
2026-07-22 03:48:16 +0800

    Refresh PWA cache for v29

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'coffee-map-v28-20260722-remove-public-transit';
+const CACHE = 'coffee-map-v29-20260722-positron-3d';
 const SHELL = [
   './', './index.html', './fonts.css?v=17', './styles.css?v=17', './admin.css?v=17',
   './filter-scroll.css?v=17', './compact-nav.css?v=17', './city-list-fix.css?v=17',
   './map-config.js?v=17', './multicity.js?v=17', './compact-search.js?v=17',
-  './app.js?v=28', './admin-core.js?v=17', './manifest.webmanifest',
+  './app.js?v=29', './admin-core.js?v=17', './manifest.webmanifest',
   './icons/mark-cafe-shops.svg',
   './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'
 ];
